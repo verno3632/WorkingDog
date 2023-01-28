ff4fdb9ed75c1c04c036c73a98b83622cd17dd96
verno3632 <verno3632@pixiv.co.jp>
2023-01-28 15:32:45 +0900

    init

---
+++ app/src/main/java/jp/developer/retia/workingdog/MainActivity.kt
@@ -0,0 +1,43 @@
+package jp.developer.retia.workingdog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.developer.retia.workingdog.ui.theme.WorkingdogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkingdogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WorkingdogTheme {
+        Greeting("Android")
+    }
+}
